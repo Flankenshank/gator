f305c5323eaaebb2ee6efe0c9628934deceb56a3
Flankenshank <dylan.date@gmail.com>
2026-06-17 20:26:00 +0200

    Added commands

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-import { setUser,readConfig } from "./config";
+import { runCommand,CommandsRegistry,registerCommand,handlerLogin } from "./commands";
 
 function main() {
-  setUser("Dylan");
-  console.log(readConfig());
+  let registry: CommandsRegistry = {};
+  registerCommand(registry, "login", handlerLogin);
+  const args = process.argv.slice(2);
+  if (args.length === 0) {
+    console.error("No command provided");
+    process.exit(1);
+  }
+  const cmdName = args[0];
+  const cmdArgs = args.slice(1);
+  try {
+    runCommand(registry, cmdName, ...cmdArgs);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error:", error.message);
+    } else {
+      console.error("Error:", error);
+    }
+    process.exit(1);
+  }
 }
 
 main();
